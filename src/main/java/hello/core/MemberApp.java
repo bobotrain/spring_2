9075a074e1a8cb2bfdefa3ddbc579661024b5979
최경보 <chlrudqh97@naver.com>
2023-06-25 23:56:49 +0900

    first commit (0625)

---
+++ src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,8 @@
+package hello.core;
+
+public class MemberApp {
+    //psvm + enter 단축키
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+    }
+}
