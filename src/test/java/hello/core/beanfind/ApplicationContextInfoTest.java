f191c353a65c01bfd811b50000043b8cf0356ef0
최경보 <chlrudqh97@naver.com>
2023-08-08 12:14:29 +0900

    0808 Bean 조회하기 (유형별)

--- src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -4,6 +4,7 @@ import hello.core.AppConfig;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 class ApplicationContextInfoTest {
@@ -19,6 +20,24 @@ class ApplicationContextInfoTest {
             //alt + enter 하면 object가 나옴 -> 타입을 지정하지 않았기 때문에
             Object bean = ac.getBean(beanDefinitionName);
             System.out.println("Name = " + beanDefinitionName + " object = " + bean);
+        }
+    }
+
+    @Test
+    @DisplayName("애플리케이션 빈 출력하기")
+    void findApplicationBean() {
+        //alt + enter
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        //iter + tab 하면 for문 자동완성
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            //Role ROLE_APPLICATION :  직접 등록한 애플리케이션 빈
+            //Role ROLE_INFRASTRUCTURE : 스프링이 내부에서 사용하는 빈
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_INFRASTRUCTURE  ){
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("Name = " + beanDefinitionName + " object = " + bean);
+            }
         }
     }
 }
