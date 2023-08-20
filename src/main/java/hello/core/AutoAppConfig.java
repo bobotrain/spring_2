99e4d16d70447d20957873d4d50685de41a82d44
최경보 <chlrudqh97@naver.com>
2023-08-20 23:40:12 +0900

    20230820 자동빈등록  vs  수동빈 등록 우선순위

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,6 +1,9 @@
 package hello.core;
 
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -15,5 +18,10 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+    @Bean
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 
 }
